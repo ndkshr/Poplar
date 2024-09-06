6109df758e63df15c4f35020746c5d86235f2d0c
Nandu <nandugops@gmail.com>
2024-09-05 20:57:24 -0700

    Adds Rich editor for jetpack compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("com.mohamedrejeb.richeditor:richeditor-compose:1.0.0-rc06")
 }
